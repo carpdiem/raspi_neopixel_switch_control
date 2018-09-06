b45d0b405d461ed3ab91b321195b75c44f6d7ae1
Michael Woods <mail@michaelwoods.us>
2018-09-05 20:38:57 -0700

    more runtime fixes

--- switch_control.py
+++ switch_control.py
@@ -1,6 +1,7 @@
 import gpiozero as g
 import numpy as np
 import requests as r
+from time import sleep
 
 def switch_turning_on():
     r1 = requests.post("http://northwall-bedroom:5000/lights_on", data = {'color_temp': '3300', 'brightness': '0.5'})
@@ -16,4 +17,4 @@ if __name__ == "__main__":
     switch_pin.when_activated = switch_turning_on
     switch_pin.when_deactivated = switch_turning_off
     while True:
-
+        sleep(1)

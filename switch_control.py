ee9b54284f346e45b7598ebc012feb6e205fd9f9
Michael Woods <mail@michaelwoods.us>
2018-09-05 20:41:37 -0700

    sytax fix

--- switch_control.py
+++ switch_control.py
@@ -4,12 +4,12 @@ import requests as r
 from time import sleep
 
 def switch_turning_on():
-    r1 = requests.post("http://northwall-bedroom:5000/lights_on", data = {'color_temp': '3300', 'brightness': '0.5'})
-    r2 = requests.post("http://southwall-bedroom:5000/lights_on", data = {'color_temp': '3300', 'brightness': '0.5'})
+    r1 = r.post("http://northwall-bedroom:5000/lights_on", data = {'color_temp': '3300', 'brightness': '0.5'})
+    r2 = r.post("http://southwall-bedroom:5000/lights_on", data = {'color_temp': '3300', 'brightness': '0.5'})
 
 def switch_turning_off():
-    r1 = requests.post("http://northwall-bedroom:5000/lights_off")
-    r2 = requests.post("http://southwall-bedroom:5000/lights_off")
+    r1 = r.post("http://northwall-bedroom:5000/lights_off")
+    r2 = r.post("http://southwall-bedroom:5000/lights_off")
 
 
 if __name__ == "__main__":

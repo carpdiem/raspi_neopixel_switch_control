6ba8e23649db8a473ff6bc7f9c9620034438b488
Michael Woods <mail@michaelwoods.us>
2018-09-06 01:02:32 -0700

    bugfix

--- switch_control_launcher.sh
+++ switch_control_launcher.sh
@@ -7,4 +7,4 @@ else
 	echo "Failed to find switch control program"
 	echo "Trying to restart"
 	echo date
-	python /home/pi/raspi_neopixel_switch_control/switch_control.py
+	python /home/pi/raspi_neopixel_switch_control/switch_control.py &
